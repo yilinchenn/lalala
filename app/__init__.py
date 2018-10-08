c41d643d1b3dad62cd8cbcfe2ecb3f9befb359f8
max <maxsheng101@gmail.com>
2018-10-07 23:56:49 -0400

    add db config

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,28 @@
 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-webapp = Flask(__name__)
+from config import app_config
+
+
+#webapp = Flask(__name__)
+
+
+db = SQLAlchemy()
+
+
+config_name = os.getenv('FLASK_CONFIG')
+webapp = Flask(__name__, instance_relative_config=True)
+webapp.config.from_object(app_config[config_name])
+webapp.config.from_pyfile('config.py')
+db.init_app(webapp)
+
 
 from app import main
 from app import login
 from app import user
-
-
 
 
 
